7c9a8e25b48ec9c8a81b3c6e3020439bc1be541e
ybhaw <vaibhawofficial@gmail.com>
2025-04-14 00:03:06 +0530

    Added tests for nsw

--- vector_db/vector_db/algorithms/nsw.py
+++ vector_db/vector_db/algorithms/nsw.py
@@ -16,22 +16,47 @@ class Node:
         self.id = _incrementor
         _incrementor += 1
 
+    def add_neighbor(self, node):
+        if node == self:
+            return
+        if node not in self.neighbors:
+            self.neighbors.append(node)
+            node.add_neighbor(self)
+
+    def __repr__(self):
+        return f"Node(id={self.id}, value={self.value})"
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+        return self.id == other.id
+
+    def __lt__(self, other):
+        if not isinstance(other, Node):
+            return False
+        return self.id < other.id
+
 
 class NSW:
     def __init__(self):
         self.nodes = []
 
-    def insert(self, value: list[float]):
+    def insert(self, value: list[float]) -> Node:
         node = Node(value)
         if not self.nodes:
             self.nodes.append(node)
-            return
-        all_neighbours = [self._find_closest(value)]
+            return node
+        all_neighbours = [self._find_closest(node)]
         i = 0
         while i < len(all_neighbours):
             current_node = all_neighbours[i]
-            node.neighbors.append(current_node)
-            current_node.neighbors.append(node)
+            node.add_neighbor(current_node)
             for neighbor in current_node.neighbors:
                 if (
                     neighbor not in all_neighbours
@@ -72,7 +97,10 @@ class NSW:
         return closest
 
     def _distance_compare(self, node1: Node, node2: Node, /):
-        return sum((a - b) ** 2 for a, b in zip(node1.value, node2.value))
+        square = 0
+        for a, b in zip(node1.value, node2.value):
+            square += (a - b) ** 2
+        return square
 
     def _compare_a_to_c_and_a_to_b_to_c(self, a: Node, b: Node, c: Node) -> bool:
         """
@@ -82,8 +110,7 @@ class NSW:
         """
         a_to_c = self._distance_compare(a, c) ** 0.5
         a_to_b = self._distance_compare(a, b) ** 0.5
-        b_to_c = self._distance_compare(b, c) ** 0.5
-        return a_to_c <= a_to_b + b_to_c
+        return a_to_c <= a_to_b
 
     def search(self, vector: List[float], k: int = 1) -> List[List[float]]:
         if not self.nodes:
--- vector_db/vector_db/algorithms/test_nsws.py
+++ vector_db/vector_db/algorithms/test_nsws.py
@@ -14,3 +14,65 @@ def test_Should_add_new_node_When_no_nodes_present():
     assert node.value == value
     assert node.neighbors == []
     assert nsw.search(value, 1) == [[1.0, 2.0, 3.0]]
+
+
+def test_Should_pass_When_multiple_nodes_are_address():
+    # Arrange
+    nsw = NSW()
+    value1 = [1.0, 2.0, 3.0]
+    value2 = [4.0, 5.0, 6.0]
+    value3 = [7.0, 8.0, 9.0]
+
+    # Act
+    node1 = nsw.insert(value1)
+    assert len(nsw.nodes) == 1
+    assert node1.neighbors == []
+
+    node2 = nsw.insert(value2)
+    assert len(nsw.nodes) == 2
+    assert node1.neighbors == [node2]
+    assert node2.neighbors == [node1]
+
+    node3 = nsw.insert(value3)
+    assert len(nsw.nodes) == 3
+    assert node1.neighbors == [node2]
+    assert sorted(node2.neighbors) == sorted([node3, node1])
+    assert node3.neighbors == [node2]
+
+
+def test_Should_not_add_farther_node_as_index_When_distance_is_greater():
+    # Arrange
+    nsw = NSW()
+    value1 = [1.0, 0.0]
+    value2 = [4.0, 0.0]
+    value3 = [7.0, 1.0]
+
+    # Act
+    node1 = nsw.insert(value1)
+    node2 = nsw.insert(value2)
+    node3 = nsw.insert(value3)
+
+    # Assert
+    assert len(nsw.nodes) == 3
+    assert node1.neighbors == [node2]
+    assert sorted(node2.neighbors) == sorted([node1, node3])
+    assert node3.neighbors == [node2]
+
+
+def test_Should_get_nearest_neighbors_When_searched():
+    # Arrange
+    nsw = NSW()
+    value1 = [1.0, 2.0, 3.0]
+    value2 = [4.0, 5.0, 6.0]
+    value3 = [7.0, 8.0, 9.0]
+    nsw.insert(value1)
+    nsw.insert(value2)
+    nsw.insert(value3)
+
+    # Act
+    neighbors = nsw.search([3.5, 5.5, 6.5], 2)
+
+    # Assert
+    assert len(neighbors) == 2
+    assert neighbors[0] == [4.0, 5.0, 6.0]
+    assert neighbors[1] == [7.0, 8.0, 9.0]
